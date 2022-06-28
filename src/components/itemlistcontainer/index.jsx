8aa427aec2c15d9846a1d2e3a9cf643a7d191af2
jcdupre <jjcdupre@gmail.com>
2022-06-28 17:19:52 -0300

    Contador con boton

--- src/components/itemlistcontainer/index.jsx
+++ src/components/itemlistcontainer/index.jsx
@@ -1,9 +1,19 @@
+import ItemCount from "../ItemCount";
 import react from "react";
 import Title from "../Title";
 
 export const ItemListContainer = () => {
+    
+    const onAdd = (quantity) => {
+        console.log('Compraste ${quantity} unidades');
+    }
+
     return (
+        <>
         <title greeting="Juan" />
+        <ItemCount initial={1} stock={5} onAdd={onAdd} />
+        </>
+        
      )
 }
 
